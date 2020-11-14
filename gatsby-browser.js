25754691f606d74d04ea7e44a42cf7dae865fe53
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2020-11-14 21:49:03 +0900

    conf: prismで行番号表示

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,5 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-coy.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
