dd4ea7a59b44e3f09101d4f98b321fa0a25733fb
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2021-09-26 20:28:21 +0900

    :fire: 既存のテーマを削除

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-
-import "prismjs/themes/prism-coy.css"
-import "prismjs/plugins/line-numbers/prism-line-numbers.css"
